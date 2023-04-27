43a6700e9dc11c31b46b78c4a225f094bf415923
ColbsLamb <colbyministry@gmail.com>
2023-04-26 19:27:41 -0500

    end of session 7

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,29 +1,26 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponents";
-import { Product } from "../../app/models/product";
-import { useStoreContext } from "../../app/context/StoreContext";
 import { LoadingButton } from "@mui/lab";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { addBasketItemAsync, removeBasketItemAsync } from "../basket/basketSlice";
+import { fetchProductAsync, productSelectors } from "./catalogSlice";
 
 export default function ProductDetails() {
-const {basket, setBasket, removeItem} = useStoreContext();
-const {id} = useParams<{id: string}>();
-const [product, setProduct] = useState<Product | null>(null);
-const [loading, setLoading] = useState(true);
+const { basket, status } = useAppSelector(state => state.basket);
+const dispatch = useAppDispatch();
+const { id } = useParams<{ id: string }>();
+const product = useAppSelector(state => productSelectors.selectById(state, id));
+const {status: productStatus} = useAppSelector(state => state.catalog);
 const [quantity, setQuantity] = useState(0);
-const [submitting, setSubmitting] = useState(false);
 const item = basket?.items.find(i => i.productId === product?.id);
 
     useEffect(() => {
         if (item) setQuantity(item.quantity);
-        agent.Catalog.details(parseInt(id))
-            .then(response => setProduct(response))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-    }, [id, item])
+        if (!product && id) dispatch(fetchProductAsync(parseInt(id)));
+    }, [id, item, dispatch, product])
 
     function handleInputChange(event: any) {
         if (event.target.value >= 0) {
@@ -32,23 +29,16 @@ const item = basket?.items.find(i => i.productId === product?.id);
     }
 
     function handleUpdateCart() {
-        setSubmitting(true);
         if (!item || quantity > item.quantity) {
-            const updateQuantity = item ? quantity - item.quantity : quantity; 
-            agent.Basket.addItem(product?.id!, updateQuantity)
-                .then(basket => setBasket(basket))
-                .catch(error => console.log(error))
-                .finally(() => setSubmitting(false))
+            const updatedQuantity = item ? quantity - item.quantity : quantity; 
+            dispatch(addBasketItemAsync({productId: product?.id!, quantity: updatedQuantity}))
         } else {
             const updatedQuantity = item.quantity - quantity; 
-            agent.Basket.removeItem(product?.id!, updatedQuantity)
-                .then(() => removeItem(product?id!, updatedQuantity))
-                .catch(error => console.log(error))
-                .finally(() => setSubmitting(false));
+            dispatch(removeBasketItemAsync({productId: product?.id!, quantity: updatedQuantity}))
         }
     }
 
-    if (loading) return <LoadingComponent message='Loading product...'/>
+    if (productStatus.includes('pending')) return <LoadingComponent message='Loading product...'/>
 
     if (!product) return <NotFound />
 
@@ -96,8 +86,8 @@ const item = basket?.items.find(i => i.productId === product?.id);
                     </Grid>
                     <Grid item xs={6}>
                         <LoadingButton
-                            disabled={item?.quantity === quantity || !item && quantity === 0}
-                            loading={submitting}
+                            disabled={item?.quantity === quantity}
+                            loading={status.includes('pendingRemoveItem')}
                             onClick={handleUpdateCart}
                             sx={{height: '55px'}}
                             color='primary'
